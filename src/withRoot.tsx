0d2a848d966baa66ef3f38560f89fd25124704b2
faraway <hzylovelyl@gmail.com>
2018-07-15 22:17:06 +0800

    Initial Commit

---
+++ src/withRoot.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { blue, grey } from "@material-ui/core/colors";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: grey
+  }
+});
+
+function withRoot<T>(Component: React.ComponentType<T>) {
+  function WithRoot(props: T) {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
